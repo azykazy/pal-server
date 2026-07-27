6643fe16c1f6fce2e565f989f5292b593910197d
azykazy <azkaz12321@yahoo.co.jp>
2026-07-27 19:08:26 +0900

    fix(terraform): keyvault.tf のパージ保護を有効化（#31）

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -10,7 +10,7 @@ resource "azurerm_key_vault" "main" {
   sku_name            = "standard"
 
   rbac_authorization_enabled = true
-  purge_protection_enabled   = false
+  purge_protection_enabled   = true
   soft_delete_retention_days = 7
 }
 
